692d0bc8f20b6bfdaa0ba530cac00948d24d79ff
William Domingos <william.viana2018@hotmail.com>
2022-07-07 22:07:59 -0300

    adição aula 8 + 9

--- Aula+7/Aula 7/scripts/controller/CalcController.js
+++ Aula+7/Aula 7/scripts/controller/CalcController.js
@@ -3,8 +3,25 @@ class CalcController {
     constructor() {
 
         this.displayCalc = "0";
-        this._dataAtual;
+        this._dataAtual;  //O underscore _ indica atributo privado
 
+    }
+
+    get displayCalc() {
+        return this.displayCalc;
+
+    }
+
+    set displayCalc(valor){
+        this.displayCalc = valor;
+    }
+
+    get dataAtual() {
+        return this._dataAtual;
+    }
+
+    set dataAtual(valor) {
+        return this._dataAtual;
     }
 
 }
